2d152585f77cb17eb6dbcea482c103df2e44e11d
Vivek Singh <vivek@happay.in>
2016-11-14 02:30:19 +0530

    completed chapter 1

---
+++ Chapter-1/9_string_rotation.py
@@ -0,0 +1,31 @@
+# Assume you have a method is_substring which checks,
+# if one word is a substring of another. Given two string,
+# s1 and s2, write code to check if s2 is a rotation of s1
+# using only one call to ss_subtring
+
+import unittest
+
+
+def is_string_rotation(string1, string2):
+    if len(string1) != len(string2) or len(string1) == 0:
+        return False
+
+    large_string = string1 + string1
+    return is_substring(large_string, string2)
+
+
+def is_substring(string1, string2):
+    if string1.find(string2) == -1:
+        return False
+    return True
+
+
+class TestStringRotation(unittest.TestCase):
+
+    def setUp(self):
+        self.true_input = ("waterbottle", "erbottlewat")
+        self.false_input = ("hello", "alloh")
+
+    def test_is_string_rotation(self):
+        self.assertTrue(is_string_rotation(self.true_input[0], self.true_input[1]))
+        self.assertFalse(is_string_rotation(self.false_input[0], self.false_input[1]))
