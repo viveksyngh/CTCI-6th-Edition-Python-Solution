64776d80387bf02095462799a9c7d885f37470ea
Vivek Singh <vivek@happay.in>
2016-11-21 00:55:20 +0530

    added solution to follow up question

--- Chapter-2/5_sum_lists.py
+++ Chapter-2/5_sum_lists.py
@@ -3,7 +3,6 @@
 # reverse order, such that the 1's digit is ate the head of the
 # list, Write a function that adds the two numbers and returns
 # the sum as linked list.
-
 import unittest
 
 class Node(object):
@@ -14,6 +13,13 @@ class Node(object):
 
 
 def sum_list(list1, list2):
+    """This solution sum two numbers repersented by linked list,
+       where each node in linked list repersent digits of numbers.
+       
+       Time Complexity: O(n)
+       Space Complexity: O(n)
+    """
+
     node1, node2  = list1, list2
     result, tail = None, None
     carry = 0
@@ -40,6 +46,94 @@ def sum_list(list1, list2):
         tail.next = node
         tail = node
     return result
+
+
+def sum_list_recursive(list1, list2, carry):
+    """This solution uses recursion to add two linked list.
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+    """
+
+    if list1 == None and list2 == None and carry == 0:
+        return None
+    val = carry
+    if list1:
+        val += list1.data
+    if list2:
+        val += list2.data
+
+    result = Node(val%10)
+
+    if list1 is not None or list2 is not None:
+        if list1 is not None:
+            list1 = list1.next
+        if list2 is not None:
+            list2 = list2.next
+        more = sum_list_recursive(list1, list2, val/10)
+        result.next = more
+    return result
+
+# Suppose digits are stored in forward order, Repeat the above problem
+# Input : (6 -> 1 -> 7) + ( 2 -> 9 -> 5)
+# Output: (9 -> 1 -> 2)
+
+def sum_lists_reverse(list1, list2):
+    """This solution some two numbers whose digits,
+        are stored in forward order. 
+        
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+    """
+
+    l1 = get_length(list1)
+    l2 = get_length(list2)
+
+    if l1 > l2:
+        list2 = pad_list(list2, l1 - l2)
+    elif l2 > l1:
+        list1 = pad_list(list1, l2 - l1)
+
+    result, carry = sum_lists_reverse_helper(list1, list2)
+
+    if carry != 0:
+        result = insert_before(result, carry)
+    return result
+
+# Helper functions
+def sum_lists_reverse_helper(list1, list2):
+    """ Helper functions to sum reversed list."""
+    if list1 is None and list2 is None:
+        return None, 0
+    result, carry = sum_lists_reverse_helper(list1.next, list2.next)
+    val = list1.data + list2.data + carry
+    final_result = insert_before(result, val%10)
+    return final_result, val/10
+
+
+def insert_before(result, val):
+    """Helper function to insert a node at begining of linked list."""
+    node = Node(val)
+    node.next = result
+    result = node
+    return result
+
+
+def get_length(list1):
+    """Helper function to get length of linked list."""
+    count = 0
+    node = list1
+    while node:
+        node = node.next
+        count += 1
+    return count
+
+
+def pad_list(list1, count):
+    """Helper function to pad the list with zero in the begining."""
+    node = list1
+    for i in range(count):
+        node = insert_before(node, 0)
+    return node
 
 
 class TestSumList(unittest.TestCase):
@@ -70,7 +164,13 @@ class TestSumList(unittest.TestCase):
     def test_sum_list_1(self):
         self.print_node(self.list1)
         self.print_node(self.list2)
-        sum_l = sum_list(self.list1, self.list2)
+        sum_l = sum_list_recursive(self.list1, self.list2, 0)
+        self.print_node(sum_l)
+
+    def test_sum_list_reverse(self):
+        self.print_node(self.list1)
+        self.print_node(self.list2)
+        sum_l = sum_lists_reverse(self.list1, self.list2)
         self.print_node(sum_l)
 
 
